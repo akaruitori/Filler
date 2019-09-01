9a942d87fc9db64d982ab66d065c8d9468435306
Dontos hollard Timeon <dtimeon@e1r11p3.21-school.ru>
2019-09-01 20:46:25 +0300

    Started with structs and very beginning of reading players data

--- filler.h
+++ filler.h
@@ -6,7 +6,7 @@
 /*   By: dtimeon <dtimeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 16:47:54 by dtimeon           #+#    #+#             */
-/*   Updated: 2019/08/30 19:42:31 by dtimeon          ###   ########.fr       */
+/*   Updated: 2019/09/01 20:05:31 by dtimeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,38 @@
 # include <unistd.h>
 # include <stdio.h>
 
+typedef struct		s_player
+{
+	char			*name;
+	char			sym;
+	char			latest_sym;
+}					t_player;
 
+typedef struct		s_pos
+{
+	int				x;
+	int				y;
+}					t_pos;
+
+typedef struct		s_map
+{
+	int				**heat_map;
+	int				width;
+	int				height;
+}					t_map;
+
+typedef struct		s_piece
+{
+	int				width;
+	int				height;
+	char			**token;
+}					t_piece;
+
+typedef struct		s_game
+{
+	t_player		*self;
+	t_player		*enemy;
+	t_map			*map;
+}					t_game;
 
 #endif
--- filler_main.c
+++ filler_main.c
@@ -6,43 +6,91 @@
 /*   By: dtimeon <dtimeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 16:47:33 by dtimeon           #+#    #+#             */
-/*   Updated: 2019/08/30 20:46:40 by dtimeon          ###   ########.fr       */
+/*   Updated: 2019/09/01 20:44:13 by dtimeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-int			filler(void)
+char		*find_player_line()
 {
 	char	*line;
-	int		success_read_flag;
-	// size_t	line_len;
 
-	// if ((success_read_flag = get_next_line(STDIN_FILENO, &line)) >= 0)
-	// {
-		// line_len = ft_strlen(line);
-		// write(1, "Success\n", 8);
-		// write(1, line, line_len);
-		success_read_flag = 0;
-		while (get_next_line(STDIN_FILENO, &line) > 0)
-		{
-			if (ft_strequ(line, "**"))
-				break;
-			success_read_flag++;
-		}
-		write(1, "8 2", 3);
-	// }
-	// if (success_read_flag < 0)
-		// return (0);
-	return (1);
+	line = NULL;
+	while(get_next_line(STDIN_FILENO, &line))
+	{
+		if (strnequ(line, "$$$", 3))
+			return (line);
+		ft_strdel(&line);
+	}
+	return (NULL);
+}
+
+int			save_player(t_game *game, char *line, char *self_name)
+{
+	char	*name_start;
+	char	*name_end;
+	int		player_no;
+
+	player_no = read_player_no(line);
+	if (!(name_start = ft_strchr(line, '[') ||
+		!(name_end = ft_strchr(name_start, ']'))))
+		return (0);
+	if (ft_strstr(name_start, self_name) && !(game->self))
+	{
+		game->self = init_self(name_start, name_end - name_start, player_no);
+	}
+	else
+	{
+		game->enemy = init_enemy(name_start, name_end - name_start, player_no);
+	}
 
 }
 
-int			main(void)
+int			read_players(t_game *game, char *self_name)
 {
-	if (!filler())
+	char	*line;
+	int		players_read;
+
+	players_read = 0;
+	while (players_read < 2)
 	{
-		perror("An error occured in filler function: ");
+		if (!(line = find_player_line()) ||
+			!(save_player(game, line, self_name)))
+			return (0);
+		players_read++;
+	}
+	return (1);
+}
+
+t_game		*init_game()
+{
+	t_game	*new;
+
+	if (!(new = (t_game *)malloc(sizeof(t_game))))
+		return (NULL);
+	new->map = NULL;
+	new->self = NULL;
+	new->enemy = NULL;
+	return (new);
+}
+
+int			filler(char	*self_name)
+{
+	t_game	*game;
+
+	game = init_game();
+	if (!game || !read_players(game, self_name))
+		return (0);
+
+	return (1);
+}
+
+int			main(int ac, char **av)
+{
+	if (!filler(av[0]))
+	{
+		perror("An error occured in filler function.");
 		return (1);
 	}
 	return (0);
